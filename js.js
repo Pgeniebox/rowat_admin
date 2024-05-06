36236fe2b648418d7e7c02cd62c5a3b2b2217463
Pgeniebox <140095988+Pgeniebox@users.noreply.github.com>
2024-05-06 13:42:21 +0000

    h

--- js.js
+++ js.js
@@ -1,6 +1,1 @@
-let message='none';
-setInterval(() => {
-     const timestamp = new Date().getTime();
-    importScripts(`js.js?t=${timestamp}`);
-postMessage(message);
-}, 5000);
+message='none'; setInterval(() => {      const timestamp = new Date().getTime();     importScripts(`js.js?t=${timestamp}`); postMessage(message); }, 5000);
